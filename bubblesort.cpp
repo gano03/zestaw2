a65b98d961f513ae44c47d95193279202658823b
pawelganobis <pganobis@interia.pl>
2021-11-22 14:56:38 +0100

    final

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
-
 using namespace std;
 template <typename T>
-void bubble (T* ptr, T n)
+void bubble (T* ptr, int left, int right)
 {
-    for(int i=0;i<n;i++)
+    for(int i=left;i<right;i++)
     {
-		for(int j=1;j<n-i;j++)
+		for(int j=left+1;j<right-i;j++)
         {
 		    if(ptr[j-1]>ptr[j])
             {
@@ -17,8 +16,6 @@ void bubble (T* ptr, T n)
             }
         }
     }
-    
-
 }
 
 
@@ -27,8 +24,7 @@ int main (int argc, char *argv[])
     int n = 0;
     int ptr[] = {5,2,13,8,6,19,1,4,0,10,8, 77};
     n = sizeof (ptr) / 4;
-    cout << n <<"\n";
-    bubble (ptr, n);
+    bubble (ptr,0, n);
     for (int i = 0; i < n; i++)
     {
         cout << " "<< ptr[i] << " ";
--- merge.cpp
+++ merge.cpp
@@ -3,20 +3,20 @@
 #include <cstdlib>
 using namespace std;
 //int *pomc;
-template <typename Iterator>
-void scal (Iterator lewy, Iterator srodek, Iterator prawy, Iterator ptr[], int n)
+template <typename T>
+void scal (int left, int middle,int right, T ptr[])
 {
-    Iterator pomc[n];
-    for (int i = lewy; i <= prawy; i++)
+    T pomc[right];
+    for (int i = left; i <= right; i++)
     {
         pomc[i] = ptr[i];
     }
-    Iterator le = lewy;
-    Iterator sr = srodek + 1;
-    for(int k=lewy; k<=prawy; k++)
+    int le = left;
+    int sr = middle + 1;
+    for(int k=left; k<=right; k++)
     {
-        if(le<=srodek)
-            if(sr <= prawy)
+        if(le<=middle)
+            if(sr <= right)
                 if(pomc[sr]<pomc[le])
                     ptr[k] = pomc[sr++];
                 else
@@ -27,18 +27,18 @@ void scal (Iterator lewy, Iterator srodek, Iterator prawy, Iterator ptr[], int n
             ptr[k] = pomc[sr++];
     }
 }
-template <typename Iterator>
-void sort (Iterator lewy, Iterator prawy, Iterator ptr[], int n)
+template <typename T>
+void sort (T *ptr, int left, int right)
 {
     
-    if (prawy <= lewy)
+    if (right <= left)
     {
         return;
     }
-    Iterator srodek = (prawy + lewy) / 2;
-    sort (lewy, srodek, ptr, n);
-    sort (srodek+1, prawy, ptr, n);
-    scal (lewy, srodek, prawy, ptr, n);
+    int middle = (right + left) / 2;
+    sort (ptr,left, middle);
+    sort (ptr, middle+1, right);
+    scal (left, middle, right, ptr);
 
 }
 
@@ -46,7 +46,7 @@ int main (int argc, char* argv[])
 {   
     int ptr[] =  { 5,2,13,8,6,19,1,4,0,15,8,77 };
     int n = sizeof (ptr) /4;
-    sort (0, n-1 ,ptr, n);
+    sort (ptr,0,n-1);
     cout << "Koniec \n";
     for (int i = 0; i < n; i++)
     {
